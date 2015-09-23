15c0ca33441f39d81eaabde777457bb43e65ac4f
Bo Xin <bxin@lsst.org>
2015-09-22 22:32:52 -0700

    add cwfsErrors.py

---
+++ source/cwfsErrors.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+##
+# @package cwfs
+# @file cwfsImage.py
+##
+# @authors: Bo Xin & Chuck Claver
+# @       Large Synoptic Survey Telescope
+
+
+class nonSquareImageError(Exception):
+
+    def __init__(self):
+        pass
+
+
+class imageDiffSizeError(Exception):
+
+    def __init__(self):
+        pass
